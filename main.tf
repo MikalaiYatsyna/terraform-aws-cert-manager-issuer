200854a0654fa9e7cb05a6edd62ddb7b8c8acbcb
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-18 11:33:48 +0400

    Add CA issuer to use for internal tls (#5)

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-resource "kubernetes_manifest" "acme_issuer" {
+resource "kubernetes_manifest" "letsencrypt_issuer" {
   manifest = {
     "apiVersion" = "cert-manager.io/v1"
     "kind"       = "ClusterIssuer"
     "metadata" = {
-      "name" = "${var.stack}-acme-issuer"
+      "name" = "${var.stack}-letsencrypt-issuer"
     }
     "spec" = {
       "acme" = {
         "email"  = var.email
-        "server" = var.acme_server
+        "server" = var.letsencrypt_server
         "privateKeySecretRef" = {
-          "name" = "letsencrypt-${var.stack}"
+          "name" = "${var.stack}-letsencrypt-private-key"
         }
         "solvers" = [
           {
@@ -27,3 +27,59 @@ resource "kubernetes_manifest" "acme_issuer" {
   }
 }
 
+resource "kubernetes_manifest" "self_signed_issuer" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Issuer"
+    "metadata" = {
+      "name"      = "${var.namespace}-self-signed-issuer"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "selfSigned" = {}
+    }
+  }
+}
+
+resource "kubernetes_manifest" "core_ca" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "${var.namespace}-ca"
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "isCA"       = true
+      "secretName" = "ca-secret"
+      "issuerRef" = {
+        "name" = kubernetes_manifest.self_signed_issuer.manifest.metadata.name
+        "kind" = "Issuer"
+      }
+      "usages" = ["server auth", "client auth"]
+      "dnsNames" = [
+        var.namespace,
+        "${var.namespace}.svc",
+        "${var.namespace}.svc.cluster",
+        "${var.namespace}.svc.cluster.local"
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ca_issuer" {
+  depends_on = [kubernetes_manifest.core_ca]
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Issuer"
+    "metadata" = {
+      "name"    = "${var.stack}-ca-issuer"
+      namespace = var.namespace
+    }
+    "spec" = {
+      "ca" = {
+        secretName = kubernetes_manifest.core_ca.manifest.spec.secretName
+      }
+    }
+  }
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "name" {
-  value = kubernetes_manifest.acme_issuer.manifest.metadata["name"]
+output "letsencrypt_issuer" {
+  value = kubernetes_manifest.letsencrypt_issuer.manifest.metadata.name
+}
+output "ca_issuer" {
+  value = kubernetes_manifest.ca_issuer.manifest.metadata.name
 }
