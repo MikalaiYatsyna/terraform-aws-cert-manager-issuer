a6a09df6159361d18ca351d96d5c7b5c8dfe07b5
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-19 13:44:29 +0400

    Remove CA issuer
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- variables.tf
+++ variables.tf
@@ -23,8 +23,3 @@ variable "letsencrypt_server" {
   description = "Letsencrypt server. Use prod by default. Override to staging for test"
   default     = "https://acme-v02.api.letsencrypt.org/directory"
 }
-
-variable "namespace" {
-  type        = string
-  description = "Namespace where to store CA"
-}
