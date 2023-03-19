31058787060200733a6e4e6ffc7f24cd2793566f
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-19 13:45:44 +0400

    Remove CA issuer (#6)

--- output.tf
+++ output.tf
@@ -1,6 +1,3 @@
 output "letsencrypt_issuer" {
   value = kubernetes_manifest.letsencrypt_issuer.manifest.metadata.name
 }
-output "ca_issuer" {
-  value = kubernetes_manifest.ca_issuer.manifest.metadata.name
-}
